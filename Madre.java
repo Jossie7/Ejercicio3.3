bbd0069c106d7825c934ce5b8ebf76fa97fb5b93
Jossie7 <115530619+Jossie7@users.noreply.github.com>
2022-12-13 19:07:39 +0100

    Add files via upload

---
+++ Madre.java
@@ -0,0 +1,8 @@
+public class Madre extends Abuela {
+    void ComidaPreferida(){
+        System.out.println("la comida preferida de mi madre es " + comidafav);
+    }
+    void MostrarNombre(){
+        System.out.println("El nombre de mi madre es " + nombre);
+    }
+}
